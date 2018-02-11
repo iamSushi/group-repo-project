1bee2d20bf9e942c6dae30775452a783458a3a8f
iamSushi <15758902+iamSushi@users.noreply.github.com>
2018-02-11 15:09:02 +0800

    admin.php
    adding land page of admin

--- assets2/admin.php
+++ assets2/admin.php
@@ -16,40 +16,38 @@
 	</head>
 	<body>
 	    <header class="admin-header">
-	    	<div class="admin-header-admin">
-	    		<label for="" style="color:white;">Admin ID</label>
-	    		<button class="btn btn-default admin-header-button">Logout</button>
-	    	</div>
-	    	<div>
-				<nav>
-				 	<ul class="admin-nav-ul">
-						 <li><a href="#">Staff</a></li>
-						 <li><a href="#">Contact</a></li>
-						 <li><a href="#">About</a></li>
-						 <li><a href="#">Salary_report</a></li>
-						 <li><a href="#">Deduction</a></li>
-						 <li><a href="#">Department</a></li>
-						 <li><a href="#"></a></li>
-						 <li><a href="#"></a></li>
-					</ul>
-				</nav>
+	    	<div  class="admin-header-admin"> 
+	    		<div>
+		    		<label for="" style="color:white;">Admin ID</label>
+		    		<div  class="admin-header-button">
+			    		<input type="text" placeholder="Search">
+			    		<button type="button">SR</button>
+		    			<button type="button">Logout</button>
+	    			</div>
+	    		</div>
 			</div>
 	    </header>
 	    <main>
-	         <div style="margin-left:25%;padding:1px 16px;height:1000px;">
-			  <h2>Fixed Full-height Side Nav</h2>
-			  <h3>Try to scroll this area, and see how the sidenav sticks to the page</h3>
-			  <p>Notice that this div element has a left margin of 25%. This is because the side navigation is set to 25% width. If you remove the margin, the sidenav will overlay/sit on top of this div.</p>
-			  <p>Also notice that we have set overflow:auto to sidenav. This will add a scrollbar when the sidenav is too long (for example if it has over 50 links inside of it).</p>
-			  <p>Some text..</p>
-			  <p>Some text..</p>
-			  <p>Some text..</p>
-			  <p>Some text..</p>
-			  <p>Some text..</p>
-			  <p>Some text..</p>
-			  <p>Some text..</p>
+	    	<div>
+		    	<aside>
+		    		<nav>
+					 	<ul class="admin-nav-ul">
+							 <li><a href="#">Staff</a></li>
+							 <li><a href="#">Contact</a></li>
+							 <li><a href="#">About</a></li>
+							 <li><a href="#">Salary_report</a></li>
+							 <li><a href="#">Deduction</a></li>
+							 <li><a href="#">Department</a></li>
+							 <li><a href="#"></a></li>
+							 <li><a href="#"></a></li>
+						</ul>
+					</nav>
+		    	</aside>
+		        <section class="content">
+				   
+				</section>
 			</div>
-				    </main>
+		</main>
 	    <footer>
 
 	    </footer>
